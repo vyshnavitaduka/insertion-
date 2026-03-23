cf56a2bb39f337473f45551da848d3a08fce15fd
Vyshnavi <vyshnavitaduka@gmail.com>
2026-03-23 21:11:03 +0530

    Added new files

--- Insertion.java
+++ Insertion.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class Insertion {
-    
-}
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i=1;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+    for(int i = 0; i < n; i++){
+        System.out.print(arr[i] + " ");
+    }
+  }
+}  
+
